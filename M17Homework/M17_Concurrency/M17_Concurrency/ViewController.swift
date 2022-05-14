1d98812c2b060e14e8064ae3f5919efa81e778f5
Dudareva <dudareva.yana@outlook.com>
2022-05-14 18:47:26 +0300

    M17 Homework

--- M17Homework/M17_Concurrency/M17_Concurrency/ViewController.swift
+++ M17Homework/M17_Concurrency/M17_Concurrency/ViewController.swift
@@ -6,44 +6,80 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
     let service = Service()
+    var imageArray: [UIImageView] = []
     
-    private lazy var imageView: UIImageView = {
-        let view = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-        view.contentMode = .scaleAspectFit
-        return view
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        return stackView
     }()
     
     private lazy var activityIndicator: UIActivityIndicatorView = {
-        let view = UIActivityIndicatorView(frame: CGRect(x: 220, y: 220, width: 140, height: 140))
+        let view = UIActivityIndicatorView(style: .large)
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(imageView)
-        view.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
+        setupViews()
+        setupConstraints()
+        
         onLoad()
     }
 
     private func onLoad() {
-        service.getImageURL { urlString, error in
-            guard
-                let urlString = urlString
-            else {
-                return
+        let dispatchGroup = DispatchGroup()
+        
+        for _ in 0...3 {
+            dispatchGroup.enter()
+            service.getImageURL { [weak self] urlString, error in
+                guard let self = self else { return }
+                guard let urlString = urlString else { return }
+                DispatchQueue.main.async(group: dispatchGroup) { [weak self] in
+                    guard let self = self else { return }
+                    sleep(arc4random() % 4)
+                    let image = self.service.loadImage(urlString: urlString)
+                    let imageView = UIImageView(image: image)
+                    imageView.contentMode = .scaleAspectFit
+                    self.imageArray.append(imageView)
+                    dispatchGroup.leave()
+                }
             }
             
-            let image = self.service.loadImage(urlString: urlString)
-            self.imageView.image = image
-            
-            self.activityIndicator.stopAnimating()
+            dispatchGroup.notify(queue: DispatchQueue.main) { [weak self] in
+                guard let self = self else {return}
+                self.activityIndicator.stopAnimating()
+                self.stackView.removeArrangedSubview(self.activityIndicator)
+                for i in 0...3 {
+                    self.stackView.addArrangedSubview(self.imageArray[i])
+                }
+            }
         }
+        
+    }
+    
+    private func setupViews() {
+        view.addSubview(stackView)
+    }
+    
+    private func setupConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottomMargin)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        stackView.addArrangedSubview(activityIndicator)
+        activityIndicator.startAnimating()
     }
 }
-
